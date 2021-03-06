03dedbed0d5294856ce1365bbea182832010b4de
s336154 <70687346+s336154@users.noreply.github.com>
2021-03-06 20:51:08 +0100

    Initial commit

---
+++ src/main/java/oslomet/webprog/HeiController.java
@@ -0,0 +1,13 @@
+package oslomet.webprog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeiController {
+    @GetMapping("/")
+    public String hei (String navn) {
+        return "Hei verden " + navn + "!";
+    }
+
+}

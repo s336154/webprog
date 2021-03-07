a67816c6970586b9f21baea982ce2fdc79d75d91
s336154 <70687346+s336154@users.noreply.github.com>
2021-03-07 06:54:05 +0100

    system properties replaced

--- src/main/java/oslomet/webprog/KundeController.java
+++ src/main/java/oslomet/webprog/KundeController.java
@@ -4,12 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HeiController {
+public class KundeController {
     @GetMapping("/")
-    public String hei (String navn) {
-        return "Hei verden " + navn + "!";
+    public Kunde returKunde(Kunde innKunde){
+        return innKunde;
     }
-
 }
 
 
